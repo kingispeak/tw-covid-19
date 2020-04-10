1ea85345c2dcaae9c13d5907c0ff68e08b5afb97
Sin-Fong Lyu <kingispeak@gmail.com>
2020-04-10 18:47:04 +0800

    Add echarts and Chart component

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,12 +1,54 @@
 <template>
-  <div class="chart"></div>
+  <div v-bind:id="id"></div>
 </template>
 
 <script>
 export default {
-  name: 'Chart'
+  name: 'Chart',
+  props: ['id', 'data'],
+  data() {
+    return {
+      chart: null
+    };
+  },
+  methods: {
+    drwaChart(data) {
+      this.chart = this.$echarts.init(document.getElementById(this.id));
+      this.setOption(data);
+      window.addEventListener('resize', () => {
+        this.chart.resize();
+      });
+    },
+    setOption(data) {
+      this.chart.setOption(data);
+    }
+  },
+  beforeDestroy() {
+    if (!this.chart) {
+      return;
+    }
+    window.removeEventListener('resize', () => {
+      this.chart.clear();
+      this.chart = null;
+    });
+  },
+  watch: {
+    data: {
+      handler(newVal, oldVal) {
+        if (this.chart) {
+          if (newVal) {
+            this.setOption(newVal);
+          } else {
+            this.setOption(oldVal);
+          }
+        } else {
+          this.drwaChart(this.data);
+        }
+      },
+      deep: true
+    }
+  }
 };
 </script>
 
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,16 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import echarts from 'echarts';
+// 只引入基本模板
+// import echarts from 'echarts/lib/echarts';
+// import 'echarts/lib/chart/bar';
 
 Vue.config.productionTip = false;
+Vue.prototype.$echarts = echarts;
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
+  render: h => h(App)
 }).$mount('#app');
