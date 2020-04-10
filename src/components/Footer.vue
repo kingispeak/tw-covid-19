cd0c4bb5e618a6a55d8d3b88534d44c9c4f20e07
Sin-Fong Lyu <kingispeak@gmail.com>
2020-04-10 17:49:02 +0800

    Modify components

---
+++ src/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer class="footer">
+    資料來源：
+    <a href="https://data.gov.tw/dataset/118038" target="_blank">疾管署</a>
+  </footer>
+</template>
+
+<style lang='scss'>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>台灣新冠肺炎(COVID-19)疫情數據</h1>
+    <Chart />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import Chart from '@/components/Chart.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Chart
   }
 };
 </script>
