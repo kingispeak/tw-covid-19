fb6a4b004ddf934613c8cd0b94d2e627001f648c
Sin-Fong Lyu <kingispeak@gmail.com>
2020-04-10 17:49:02 +0800

    Modify components

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="about"></div>
+  <div class="chart"></div>
 </template>
 
 <script>
 export default {
-  name: 'About'
+  name: 'Chart'
 };
 </script>
 
---
+++ src/components/Nav.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav id="nav" class="nav">
+    <router-link to="/">Home</router-link>|
+    <router-link to="/about">About</router-link>
+  </nav>
+</template>
+
+<style lang='scss'>
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
