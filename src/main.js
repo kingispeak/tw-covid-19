928bee473e83c8eba82dba54cf3395dbb9daf11e
Sin-Fong Lyu <kingispeak@gmail.com>
2020-04-13 04:47:02 +0800

    Add google analytics

--- src/main.js
+++ src/main.js
@@ -3,12 +3,20 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import echarts from 'echarts';
+import VueAnalytics from 'vue-analytics';
+
 // 只引入基本模板
 // import echarts from 'echarts/lib/echarts';
 // import 'echarts/lib/chart/bar';
 
 Vue.config.productionTip = false;
 Vue.prototype.$echarts = echarts;
+
+// Configuration VueAnalytics
+Vue.use(VueAnalytics, {
+  id: 'UA-61761333-4',
+  router
+});
 
 new Vue({
   router,
