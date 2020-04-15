8a6a14417cd0f27052fda9f9cdd861d1aa7d2b4c
Sin-Fong Lyu <kingispeak@gmail.com>
2020-04-16 02:04:33 +0800

    Update UI

--- src/main.js
+++ src/main.js
@@ -4,18 +4,23 @@ import router from './router';
 import store from './store';
 import echarts from 'echarts';
 import VueAnalytics from 'vue-analytics';
+import 'bootstrap';
+import axios from 'axios';
 
 Vue.config.productionTip = false;
 Vue.prototype.$echarts = echarts;
+Vue.prototype.$http = axios;
+
+window.axios = axios;
 
 // Configuration VueAnalytics
 Vue.use(VueAnalytics, {
   id: 'UA-61761333-4',
-  router
+  router,
 });
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount('#app');
