ad6e6fe3be9feaa3259a2963c0022b812c67f7bd
Sin-Fong Lyu <kingispeak@gmail.com>
2020-04-16 17:01:13 +0800

    Update e2e test cases

--- tests/e2e/about.test.js
+++ tests/e2e/about.test.js
@@ -4,11 +4,6 @@ import { getPageURL } from './helpers/client-functions';
 
 const url = [baseURL, '/about'].join('');
 fixture(`About Page`).page(url);
-test('Check heading is exist on about us page! ', async t => {
-  await t
-    .expect(Selector('.nav-item-home').visible)
-    .ok()
-    .click(Selector('.nav-item-home'));
-
-  await t.expect(getPageURL()).contains('/');
+test('Check heading is exist on about us page! ', async (t) => {
+  await t.expect(Selector('.navbar').visible).ok();
 });
