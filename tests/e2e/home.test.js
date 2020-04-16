b3d9182032ee707f37bb85cdf800c5f528541657
Sin-Fong Lyu <kingispeak@gmail.com>
2020-04-16 17:01:13 +0800

    Update e2e test cases

--- tests/e2e/home.test.js
+++ tests/e2e/home.test.js
@@ -4,13 +4,10 @@ import { getPageURL } from './helpers/client-functions';
 
 fixture('Home Page').page(baseURL);
 
-test('Check heading is exist on home page!', async t => {
+test('Check heading is exist on home page!', async (t) => {
   await t
     .expect(Selector('h1').innerText)
     .eql('台灣新冠肺炎(COVID-19)疫情數據')
-    .expect(Selector('.nav-item-about').visible)
-    .ok()
-    .click(Selector('.nav-item-about'));
-
-  await t.expect(getPageURL()).contains('/about');
+    .expect(Selector('.navbar').visible)
+    .ok();
 });
