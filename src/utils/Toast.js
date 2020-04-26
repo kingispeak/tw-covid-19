0be693899cda07f47419ec64dee2d5c17eca7ae9
Sin-Fong Lyu <kingispeak@gmail.com>
2020-04-26 10:58:09 +0800

    Wrapper for axios and toast

---
+++ src/utils/Toast.js
@@ -0,0 +1,19 @@
+import iZtoast from 'izitoast';
+import 'izitoast/dist/css/iziToast.css';
+
+export default {
+  error: (message, title = 'Error') => {
+    return iZtoast.error({
+      title: title,
+      message: message,
+      position: 'bottomLeft',
+    });
+  },
+  success: (message, title = 'Success') => {
+    return iZtoast.success({
+      title: title,
+      message: message,
+      position: 'bottomLeft',
+    });
+  },
+};
