4643cb8ae604805e854ccaade2377ff013de5c0b
Sin-Fong Lyu <kingispeak@gmail.com>
2020-04-29 02:50:48 +0800

    Update navbar

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="navbar navbar-expand navbar-dark bg-dark">
-    <router-link class="navbar-brand" to="/">COVID-19</router-link>
+    <span class="navbar-brand h1 mb-0">COVID-19</span>
     <ul class="navbar-nav mr-auto">
       <li class="nav-item">
         <router-link class="nav-link" to="/">Home</router-link>
@@ -9,8 +9,29 @@
         <router-link class="nav-link" to="/news">News</router-link>
       </li>
     </ul>
-    <a href="https://github.com/kingispeak/tw-covid-19" target="_blank">
-      <img src="@/assets/images/GitHub-Mark-Light-32px.png" alt="github" />
-    </a>
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a
+          class="nav-link"
+          href="https://github.com/kingispeak/tw-covid-19"
+          target="_blank"
+        >
+          <i class="fa fa-github fa-lg" aria-hidden="true"></i>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <i class="fa fa-share-alt fa-lg" aria-hidden="true"></i>
+        </a>
+      </li>
+    </ul>
   </nav>
 </template>
+
+<script>
+export default {
+  name: 'Nav'
+};
+</script>
+
+<style scoped lang="scss"></style>
