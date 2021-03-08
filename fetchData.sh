3a65c25f474111b2a215ab52414b962e6b3924f1
Sin-Fong Lyu <kingispeak@gmail.com>
2021-03-08 23:39:48 +0800

    Add fetch data bash shell

---
+++ fetchData.sh
@@ -0,0 +1,19 @@
+#!/bin/bash  
+
+# die script -- just in case
+die() { echo "$@" 1>&2 ; exit 1; }
+
+url=https://od.cdc.gov.tw/eic/Weekly_Age_County_Gender_19CoV.json
+path=./src/assets/Weekly_Age_County_Gender_19CoV.json
+
+if [ -x "$(which wget)" ] ; then
+    wget -q $url -O $path
+elif [ -x "$(which curl)" ]; then
+    curl -o $path -sfL $url
+else
+    echo "Could not find curl or wget, please install one." >&2
+fi
+
+git add $path
+git commit -m 'docs: auto update covid-19 open data'
+git push
